c12ba37ccb67f689b1170ad3ebe6b8bf4bd6752a
Ngoc Huynh <ngoc.huynh@terralogic.com>
2017-09-22 13:16:42 +0700

    Add SimpleArticle Component

---
+++ index.js
@@ -0,0 +1,66 @@
+import React, {Component, PropTypes} from 'react';
+class SimpleArticle extends Component {
+  static propTypes = {
+    FRN_rawResponses: PropTypes.array,
+    FRN_requestError: PropTypes.object
+  };
+
+  componentWillMount() {
+    if (process.env.BROWSER) {
+      const {
+        FRN_rawResponses: [
+          {
+            data: {
+              body = ''
+            } = {}
+          } = {}
+        ] = [],
+      } = this.props;
+
+      if (body) {
+        const bodyHtmlWithoutScripts = body.split(/<script[\s\S]*?>[\s\S]*?<\/script>/gi).join('');
+        this.setState({
+          bodyHtml: bodyHtmlWithoutScripts
+        });
+      }
+    }
+  }
+
+  render() {
+    const {
+      FRN_rawResponses: [
+        {
+          data: {
+            headline = '',
+            abstract = ''
+          } = {}
+        } = {}
+      ] = []
+    } = this.props;
+
+    const {
+      bodyHtml
+    } = this.state;
+
+    return (
+      <div >
+        <style dangerouslySetInnerHTML={{ __html: `
+          .SimpleArticle-headline{
+            overflow-x: hidden;
+            color: green;
+          }
+          .SimpleArticle-abstract{
+            text-decoration: overline;
+          }
+          .SimpleArticle-body{
+            transform: rotate(.5deg);
+          }
+        ` }} />
+        <h1 className="SimpleArticle-headline"> {headline} </h1>
+        <div className="SimpleArticle-abstract" dangerouslySetInnerHTML={{ __html: abstract }}></div>
+        <div className="SimpleArticle-body" dangerouslySetInnerHTML={{ __html: bodyHtml }}></div>
+      </div>
+    );
+  }
+}
+export default SimpleArticle;
